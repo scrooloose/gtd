ad60f643dbb3a042e03547cbae56a7960e0f87d7
Martin Grenfell <martin.grenfell@gmail.com>
2016-08-05 12:10:27 +0100

    fork the ding call so we dont have to wait for the sound to finish

--- lib/gtd/break_actions/ding.rb
+++ lib/gtd/break_actions/ding.rb
@@ -1,7 +1,12 @@
 module GTD::BreakActions
   class Ding < Base
     def perform
-      `paplay --volume=50000 /usr/share/sounds/freedesktop/stereo/complete.oga`
+
+      #fork this off so we dont have to wait for the sound to finish before
+      #proceeding
+      fork do
+        `paplay --volume=50000 /usr/share/sounds/freedesktop/stereo/complete.oga`
+      end
     end
   end
 end
